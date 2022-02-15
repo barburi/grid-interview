b35f95ea052da8f6692cc40b89d0d21d641d6055
barburi <62951661+barburi@users.noreply.github.com>
2022-02-15 19:14:25 +0200

    Update Grid.js

--- src/Grid.js
+++ src/Grid.js
@@ -7,17 +7,6 @@ const Grid = ({ config, data }) => (
       <th>Col 1</th>
       <th>Col 2</th>
     </tr>
-    </thead>
-    <tbody>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
-    </tbody>
   </table>
 );
 
